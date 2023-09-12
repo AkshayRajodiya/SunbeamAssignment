97dbf066f6f5d05cdebe03572c85435cfb8c6a0c
AkshayRajodiya <AkshayRajodiya.ar@gmail.com>
2023-09-12 18:52:34 +0530

    question no 7 done

--- Day2/question7.py
+++ Day2/question7.py
@@ -1,16 +1,16 @@
 # Write a program that will calculate the price for a quantity entered from the keyboard, given that the unit
-# price is Rs 5 and there is a discount of 10 percent for quantities over 30 and a 15 percent discount for qu
-# antities over 50.
+# price is Rs 5 and there is a discount of 10 percent for quantities over 30 and a 15 percent discount for quantities
+# over 50.
 def calculate_price(quantity):
     price = 5
     if quantity < 30:
-        bill_value = 5 * quantity
+        bill_value = price * quantity
         return bill_value
-    elif 50 > quantity > 30 :
-        bill_value = (5 * quantity) - ((5 * quantity)*10/100)
+    elif 50 > quantity > 30:
+        bill_value = (price * quantity) - ((price * quantity)*10/100)
         return bill_value
     else:
-        bill_value = (5 * quantity) - ((5 * quantity) * 15 // 100)
+        bill_value = (price * quantity) - ((price * quantity) * 15 // 100)
         return bill_value
 
 
@@ -20,4 +20,3 @@ result_2 = calculate_price(55)
 print(result_2)
 result_3 = calculate_price(25)
 print(result_3)
-
