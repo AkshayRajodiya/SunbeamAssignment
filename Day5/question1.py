4ee3dd2671cba033108db582ab7a84257f01c75a
AkshayRajodiya <AkshayRajodiya.ar@gmail.com>
2023-09-16 14:32:57 +0530

    "day 5 assignment question 1 done"

--- Day5/question1.py
+++ Day5/question1.py
@@ -4,22 +4,33 @@
 # B. Change Lisa’s favourite colour
 # C. Remove 'Jenny' and her favourite colour
 # D. Sort and print students and their favourite colours alphabetically by name
-def dictionary_of_stud(people):
-    list_stud = list(people.keys())
-    list_stud.sort()
-    list_color = list(people.values())
-    print(len(list_stud))   # A. Find out how many students are in the list
-    print(list_stud)
-    list_color.sort()       # D. Sort and print students and their favourite colours alphabetically by name
-    for key in people.keys():
-        if key == 'Lisa':
-            people[key] = 'red'  # B. Change Lisa’s favourite colour
-        if key == 'Jenny':
-            people[key] = None   # C. Remove 'Jenny' and her favourite colour
+def dictionary_of_stud():
+    people = {'Arham': 'Blue', 'Lisa': 'Yellow', 'Vinod': 'Purple', 'Jenny': 'Pink'}
+    # list_stud = list(people.keys())
+    # list_stud.sort()
+    # list_color = list(people.values())
+    # print(len(people))   # A. Find out how many students are in the list
+    # print(list_stud)
+    # list_color.sort()       # D. Sort and print students and their favourite colours alphabetically by name
+    # for key in people.keys():
+    #     if key == 'Lisa':
+    #         people[key] = 'red'  # B. Change Lisa’s favourite colour
+    #     if key == 'Jenny':
+    #         people[key] = None   # C. Remove 'Jenny' and her favourite colour
+    #
+    # print(list_color)
+    # print(people)
 
-    print(list_color)
+    print(len(people))  # A. Find out how many students are in the list
+    if "Lisa" in people:
+        people['Lisa'] = 'White'  # B. Change Lisa’s favourite colour
+    if "Jenny" in people:
+        people["Jenny"] = None  # C. Remove 'Jenny' and her favourite colour
+    sorted_dict = dict(
+        sorted(people.items()))  # D. Sort and print students and their favourite colours alphabetically by name
+
+    print(sorted_dict)
     print(people)
 
 
-people_list = {'Arham': 'Blue', 'Lisa': 'Yellow', 'Vinod': 'Purple', 'Jenny': 'Pink'}
-dictionary_of_stud(people_list)
+dictionary_of_stud()
